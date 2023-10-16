9478d33488761374e9fddf8ab71cf4ce9e9e3013
Christopher Santana <christopher.santana@nav.no>
2023-10-16 11:01:07 +0200

    Legger til mer i FullmaktNavnRule.

--- src/main/kotlin/no/nav/tms/varseltekst/monitor/coalesce/rules/FullmaktNavnRule.kt
+++ src/main/kotlin/no/nav/tms/varseltekst/monitor/coalesce/rules/FullmaktNavnRule.kt
@@ -1,14 +1,16 @@
 package no.nav.tms.varseltekst.monitor.coalesce.rules
 
 object FullmaktNavnRule: CoalescingRule {
-    override val description = "Tittel på brev"
+    override val description = "Navn i varsler om fullmakt"
 
     private val fullmakt = "[fF]ullmakt".toRegex()
 
-    private val gittFullmakt = "(Du har gitt fullmakt til )(.*)( og du blir nå representert av vedkommende hos NAV\\.)(.*)".toRegex()
-    private val mottattFullmakt = "(Det ble opprettet en fullmakt og du kan nå representere )(.*)(\\. Se Dine fullmakter for mer informasjon\\.)(.*)".toRegex()
-    private val avsluttGittFullmakt = "(Du har avsluttet fullmakten du tidligere har gitt til )(.*)(\\. Se Dine fullmakter for mer informasjon\\.)(.*)".toRegex()
-    private val avsluttMottattFullmakt= "(Fullmakt du har fått fra )(.*)( ble avsluttet\\.)(.*)".toRegex()
+    private val gittFullmakt = "(Du har gitt fullmakt til )(.*)( og du blir nå representert av vedkommende hos NAV)(.*)".toRegex()
+    private val mottattFullmakt = "(Det ble opprettet en fullmakt og du kan nå representere )(.*)(\\. Se Dine fullmakter for mer informasjon)(.*)".toRegex()
+    private val avsluttGittFullmakt = "(Du har avsluttet fullmakten du tidligere har gitt til )(.*)(\\. Se Dine fullmakter for mer informasjon)(.*)".toRegex()
+    private val avsluttMottattFullmakt = "(Fullmakt du har fått fra )(.*)( ble avsluttet)(.*)".toRegex()
+    private val avsluttGittLovrettet = "(Fullmakten som du har gitt til )(.*)( er opphørt av NAV)(.*)".toRegex()
+    private val avsluttMottattLovrettet = "(Fullmakt for )(.*)( er avsluttet)(.*)".toRegex()
 
     private val replacementFullmektig = "$1<fullmektig>$3$4"
     private val replacementRepresentert = "$1<representert>$3$4"
@@ -22,6 +24,8 @@ object FullmaktNavnRule: CoalescingRule {
             in mottattFullmakt -> text.replace(mottattFullmakt, replacementRepresentert)
             in avsluttGittFullmakt -> text.replace(avsluttGittFullmakt, replacementFullmektig)
             in avsluttMottattFullmakt -> text.replace(avsluttMottattFullmakt, replacementRepresentert)
+            in avsluttGittLovrettet -> text.replace(avsluttGittLovrettet, replacementFullmektig)
+            in avsluttMottattLovrettet -> text.replace(avsluttMottattLovrettet, replacementRepresentert)
             else -> text
         }
     }
--- src/test/kotlin/no/nav/tms/varseltekst/monitor/coalesce/rules/FullmaktNavnRuleTest.kt
+++ src/test/kotlin/no/nav/tms/varseltekst/monitor/coalesce/rules/FullmaktNavnRuleTest.kt
@@ -9,7 +9,7 @@ class FullmaktNavnRuleTest {
         val forventetMottatt = "Det ble opprettet en fullmakt og du kan nå representere LYKKELIG REPRESENTERT. Se Dine fullmakter for mer informasjon."
         val forventetGitt = "Du har gitt fullmakt til IVRIG REPRESENTANT og du blir nå representert av vedkommende hos NAV. Se Dine fullmakter for mer informasjon. Har du ikke opprettet en fullmakt ta kontakt på tlf."
         val forventetMottattAvsluttet = "Fullmakt du har fått fra LYKKELIG REPRESENTERT ble avsluttet. Se Dine fullmakter for mer informasjon."
-        val forventetGittAvsluttet = "Du har avsluttet fullmakten du tidligere har gitt til IVRIG REPRESENTANT. Se Dine fullmakter for mer informasjon. Har du ikke avsluttet en fullmakt ta kontakt på tlf 55553333."
+        val forventetGittAvsluttet = "Du har avsluttet fullmakten du tidligere har gitt til IVRIG REPRESENTANT. Se Dine fullmakter for mer informasjon. Har du ikke avsluttet en fullmakt ta kontakt på tlf."
         val annenPassende = "Tilfeldig tekst med nøkkelord 'fullmakt'"
 
         FullmaktNavnRule.ruleApplies(forventetMottatt) shouldBe true
@@ -31,17 +31,23 @@ class FullmaktNavnRuleTest {
         val mottatt = "Det ble opprettet en fullmakt og du kan nå representere LYKKELIG REPRESENTERT. Se Dine fullmakter for mer informasjon."
         val gitt = "Du har gitt fullmakt til IVRIG REPRESENTANT og du blir nå representert av vedkommende hos NAV. Se Dine fullmakter for mer informasjon. Har du ikke opprettet en fullmakt ta kontakt på tlf."
         val mottattAvsluttet = "Fullmakt du har fått fra LYKKELIG REPRESENTERT ble avsluttet. Se Dine fullmakter for mer informasjon."
-        val gittAvsluttet = "Du har avsluttet fullmakten du tidligere har gitt til IVRIG REPRESENTANT. Se Dine fullmakter for mer informasjon. Har du ikke avsluttet en fullmakt ta kontakt på tlf 55553333."
+        val gittAvsluttet = "Du har avsluttet fullmakten du tidligere har gitt til IVRIG REPRESENTANT. Se Dine fullmakter for mer informasjon. Har du ikke avsluttet en fullmakt ta kontakt på tlf."
+        val avsluttGittLovrettet = "Fullmakten som du har gitt til IVRIG REPRESENTANT er opphørt av NAV. Du må ta kontakt med vedkommende for årsak til dette. Du kan opprette en ny fullmakt med en annen fullmektig."
+        val avsluttMottattLovrettet = "Fullmakt for LYKKELIG REPRESENTERT er avsluttet, jf. forvaltningsloven § 12 andre ledd. Grunnen er at du er ansatt i NAV."
 
         val expectedMottatt = "Det ble opprettet en fullmakt og du kan nå representere <representert>. Se Dine fullmakter for mer informasjon."
         val expectedGitt = "Du har gitt fullmakt til <fullmektig> og du blir nå representert av vedkommende hos NAV. Se Dine fullmakter for mer informasjon. Har du ikke opprettet en fullmakt ta kontakt på tlf."
         val expectedMottattAvsluttet = "Fullmakt du har fått fra <representert> ble avsluttet. Se Dine fullmakter for mer informasjon."
-        val expectedGittAvsluttet = "Du har avsluttet fullmakten du tidligere har gitt til <fullmektig>. Se Dine fullmakter for mer informasjon. Har du ikke avsluttet en fullmakt ta kontakt på tlf 55553333."
+        val expectedGittAvsluttet = "Du har avsluttet fullmakten du tidligere har gitt til <fullmektig>. Se Dine fullmakter for mer informasjon. Har du ikke avsluttet en fullmakt ta kontakt på tlf."
+        val expetedAvsluttGittLovrettet = "Fullmakten som du har gitt til <fullmektig> er opphørt av NAV. Du må ta kontakt med vedkommende for årsak til dette. Du kan opprette en ny fullmakt med en annen fullmektig."
+        val expetedAvsluttMottattLovrettet = "Fullmakt for <representert> er avsluttet, jf. forvaltningsloven § 12 andre ledd. Grunnen er at du er ansatt i NAV."
 
         FullmaktNavnRule.applyRule(mottatt) shouldBe expectedMottatt
         FullmaktNavnRule.applyRule(gitt) shouldBe expectedGitt
         FullmaktNavnRule.applyRule(mottattAvsluttet) shouldBe expectedMottattAvsluttet
         FullmaktNavnRule.applyRule(gittAvsluttet) shouldBe expectedGittAvsluttet
+        FullmaktNavnRule.applyRule(avsluttGittLovrettet) shouldBe expetedAvsluttGittLovrettet
+        FullmaktNavnRule.applyRule(avsluttMottattLovrettet) shouldBe expetedAvsluttMottattLovrettet
     }
 
     @Test
