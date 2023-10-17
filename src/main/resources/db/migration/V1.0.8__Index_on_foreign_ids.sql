e5d68ad8c0af9947ce0859099a32dbf4fe36f395
Christopher Santana <christopher.santana@nav.no>
2023-10-17 09:48:30 +0200

    Legger til indekser på varsel-tekst id-er.

---
+++ src/main/resources/db/migration/V1.0.8__Index_on_foreign_ids.sql
@@ -0,0 +1,4 @@
+create index varsel_web_tekst_id on varsel(web_tekst);
+create index varsel_sms_tekst_id on varsel(sms_tekst);
+create index varsel_epost_tittel_id on varsel(epost_tittel);
+create index varsel_epost_tekst_id on varsel(epost_tekst);
