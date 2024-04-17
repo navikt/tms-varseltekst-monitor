d19b999e8b7d572244cca7572492edda5a2cffb3
Christopher Santana <christopher.santana@nav.no>
2024-04-17 11:20:49 +0200

    Oppdaterer gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,21 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
     kotlin("jvm").version(Kotlin.version)
-    kotlin("plugin.allopen").version(Kotlin.version)
 
-    id(Flyway.pluginId) version (Flyway.version)
     id(Shadow.pluginId) version (Shadow.version)
 
-    // Apply the application plugin to add support for building a CLI application.
     application
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 repositories {
     mavenCentral()
-    maven("https://packages.confluent.io/maven")
     maven {
         url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     }
@@ -61,8 +57,3 @@ tasks {
         }
     }
 }
-
-// TODO: Fjern følgende work around i ny versjon av Shadow-pluginet:
-// Skal være løst i denne: https://github.com/johnrengelman/shadow/pull/612
-project.setProperty("mainClassName", application.mainClass.get())
-apply(plugin = Shadow.pluginId)
