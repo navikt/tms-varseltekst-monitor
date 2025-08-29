7efa88edb6cec4c2667a374a1d03537688c04cc4
Christopher Santana <christopher.santana@nav.no>
2025-08-29 11:11:59 +0200

    Styling.

--- src/main/kotlin/no/nav/tms/varseltekst/monitor/varseltekst/ExcelWriter.kt
+++ src/main/kotlin/no/nav/tms/varseltekst/monitor/varseltekst/ExcelWriter.kt
@@ -9,7 +9,7 @@ object ExcelWriter {
     fun totaltAntallToExcelSheet(teksttype: Teksttype, totaltAntall: List<VarselTekster.TotaltAntall>): Workbook {
         val (workbook, sheet) = initWorkbook(teksttype, "Antall", "Tekst")
 
-        sheet.setColumnWidth(1, 25000)
+        sheet.setColumnWidth(1, 20000)
 
         totaltAntall.forEachIndexed { i, antall ->
             val row = sheet.createRow(i + 1)
@@ -31,9 +31,9 @@ object ExcelWriter {
     fun antallToExcelSheet(teksttype: Teksttype, totaltAntall: List<VarselTekster.DetaljertAntall>): Workbook {
         val (workbook, sheet) = initWorkbook(teksttype, "Antall", "Varseltype", "Namespace", "Appnavn", "Tekst")
 
-        sheet.setColumnWidth(2, 4000)
-        sheet.setColumnWidth(3, 6000)
-        sheet.setColumnWidth(4, 250000)
+        sheet.setColumnWidth(2, 3000)
+        sheet.setColumnWidth(3, 5000)
+        sheet.setColumnWidth(4, 20000)
 
         totaltAntall.forEachIndexed { i, antall ->
             val row = sheet.createRow(i + 1)
@@ -76,8 +76,6 @@ object ExcelWriter {
 
         val headerStyle = workbook.createCellStyle().also { style ->
             workbook.createFont().also {
-                it.fontName = "Arial"
-                it.fontHeightInPoints = 16
                 it.bold = true
             }.let {
                 style.setFont(it)
