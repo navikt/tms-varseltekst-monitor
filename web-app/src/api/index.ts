9331e90e42e2c2476d7d8019631c5e18930f68eb
Christopher Santana <christopher.santana@nav.no>
2025-09-01 14:26:56 +0200

    Fjern axios.

--- web-app/src/api/index.ts
+++ web-app/src/api/index.ts
@@ -15,15 +15,21 @@ export interface DownloadRequest {
 	filnavn: string | null
 }
 
-export function requestDownload(request: DownloadRequest): AxiosPromise {
-	return axiosInstance.post(`/api/download`, {
-		teksttype: request.teksttype,
-		detaljert: request.detaljert,
-		varseltype: request.varseltype,
-		startDato: request.startDato,
-		sluttDato: request.sluttDato,
-		inkluderStandardtekster: request.inkluderStandardtekster,
-		minimumAntall: request.minimumAntall,
-		filnavn: request.filnavn,
+export function requestDownload(request: DownloadRequest): Promise<Response> {
+	return fetch(`/api/download`, {
+		method: 'POST',
+		body: JSON.stringify({
+			teksttype: request.teksttype,
+			detaljert: request.detaljert,
+			varseltype: request.varseltype,
+			startDato: request.startDato,
+			sluttDato: request.sluttDato,
+			inkluderStandardtekster: request.inkluderStandardtekster,
+			minimumAntall: request.minimumAntall,
+			filnavn: request.filnavn
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8'
+		}
 	});
 }
