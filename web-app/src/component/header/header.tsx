67b4dcb387449ea7d40ace416116c5009457f2e0
Christopher Santana <christopher.santana@nav.no>
2025-09-02 12:47:04 +0200

    Legger til flere valg.

--- web-app/src/component/header/header.tsx
+++ web-app/src/component/header/header.tsx
@@ -4,7 +4,7 @@ import './header.css';
 export function Header() {
 	return (
 		<header className="header">
-			<h1 className="header__title">Kafka Manager</h1>
+			<h1 className="header__title">Statistikk for antall varseltekster</h1>
 		</header>
 	);
 }
