535d1bdfd1ec149fad185595bb975f110f738d6e
Christopher Santana <christopher.santana@nav.no>
2025-09-18 11:01:33 +0200

    Mer refaktorering.

---
+++ api/src/main/kotlin/no/nav/tms/varseltekst/monitor/varseltekst/ExcelWorkbookWriter.kt
@@ -0,0 +1,133 @@
+package no.nav.tms.varseltekst.monitor.varseltekst
+
+import no.nav.tms.varseltekst.monitor.varseltekst.Tekst.Innhold.*
+import org.apache.poi.ss.usermodel.CellType
+import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.xssf.usermodel.XSSFSheet
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
+
+object ExcelWorkbookWriter {
+
+    fun totaltAntallToExcelSheet(totaltAntall: TotaltAntall): Workbook {
+
+        val (workbook, sheet) = initWorkbook("Antall", teksttyper = totaltAntall.teksttyper)
+
+        totaltAntall.teksttyper.forEachIndexed { index, _ ->
+            sheet.setColumnWidth(1 + index, 25000)
+        }
+
+        totaltAntall.permutasjoner.forEachIndexed { i, permutasjon ->
+            val row = sheet.createRow(i + 1)
+
+            row.createCell(0).apply {
+                cellType = CellType.NUMERIC
+                setCellValue(permutasjon.antall.toDouble())
+            }
+
+            permutasjon.tekster.forEachIndexed { index, tekst ->
+                row.createCell(1 + index).apply {
+                    cellType = CellType.STRING
+                    val displayText = when (tekst.innhold) {
+                        Egendefinert -> tekst.tekst!!
+                        Sladdet -> "<sladdet>"
+                        Standard -> "<standardtekst>"
+                        Ubrukt -> "<ingen>"
+                    }
+                    setCellValue(displayText)
+                }
+            }
+        }
+
+        return workbook
+    }
+
+    fun antallToExcelSheet(detaljertAntall: DetaljertAntall): Workbook {
+
+        val (workbook, sheet) = initWorkbook("Antall", "Varseltype", "Namespace", "Appnavn", teksttyper = detaljertAntall.teksttyper)
+
+        sheet.setColumnWidth(2, 3000)
+        sheet.setColumnWidth(3, 5000)
+
+        detaljertAntall.teksttyper.forEachIndexed { index, _ ->
+            sheet.setColumnWidth(4 + index, 25000)
+        }
+
+        detaljertAntall.permutasjoner.forEachIndexed { i, permutasjon ->
+            val row = sheet.createRow(i + 1)
+
+            row.createCell(0).apply {
+                cellType = CellType.NUMERIC
+                setCellValue(permutasjon.antall.toDouble())
+            }
+
+            row.createCell(1).apply {
+                cellType = CellType.STRING
+                setCellValue(permutasjon.varseltype)
+            }
+
+            row.createCell(2).apply {
+                cellType = CellType.STRING
+                setCellValue(permutasjon.produsent.namespace)
+            }
+
+            row.createCell(3).apply {
+                cellType = CellType.STRING
+                setCellValue(permutasjon.produsent.appnavn)
+            }
+
+            permutasjon.tekster.forEachIndexed { index, tekst ->
+                row.createCell(4 + index).apply {
+                    cellType = CellType.STRING
+                    val displayText = when (tekst.innhold) {
+                        Egendefinert -> tekst.tekst!!
+                        Sladdet -> "<sladdet>"
+                        Standard -> "<standardtekst>"
+                        Ubrukt -> "<ingen>"
+                    }
+                    setCellValue(displayText)
+                }
+            }
+        }
+
+        return workbook
+    }
+
+    private fun initWorkbook(vararg columns: String, teksttyper: List<Teksttype>): Pair<XSSFWorkbook, XSSFSheet> {
+        val workbook = XSSFWorkbook()
+
+        val sheetName = if (teksttyper.size == 1) {
+            teksttyper.first().name
+        } else {
+            "Kombinasjon"
+        }
+
+        val sheet = workbook.createSheet(sheetName)
+
+        val header = sheet.createRow(0)
+
+        val headerStyle = workbook.createCellStyle().also { style ->
+            workbook.createFont().also {
+                it.bold = true
+            }.let {
+                style.setFont(it)
+            }
+        }
+
+        columns.forEachIndexed { i, column ->
+            header.createCell(i).apply {
+                cellStyle = headerStyle
+                setCellValue(column)
+            }
+        }
+
+        teksttyper.forEachIndexed { i, teksttype ->
+            header.createCell(i + columns.size).apply {
+                cellStyle = headerStyle
+                setCellValue(teksttype.name)
+            }
+        }
+
+        return workbook to sheet
+    }
+}
--- api/src/main/kotlin/no/nav/tms/varseltekst/monitor/varseltekst/VarseltekstQueryService.kt
+++ api/src/main/kotlin/no/nav/tms/varseltekst/monitor/varseltekst/VarseltekstQueryService.kt
@@ -1,6 +1,5 @@
 package no.nav.tms.varseltekst.monitor.varseltekst
 
-import io.ktor.server.response.*
 import kotlinx.coroutines.*
 import org.apache.poi.ss.usermodel.Workbook
 import java.time.LocalDate
@@ -21,26 +20,8 @@ class VarseltekstQueryService(
         fileStore[fileId] = ExcelFile.waiting(filename)
 
         queryScope.launch {
-            if (request.detaljert) {
-                varseltekstRepository.tellAntallVarseltekster(
-                    teksttyper = request.teksttyper,
-                    varseltype = request.varseltype,
-                    startDato = request.startDato,
-                    sluttDato = request.sluttDato,
-                    inkluderStandardtekster = request.inkluderStandardtekster
-                ).let {
-                    ExcelFileWriter.antallToExcelSheet(it, request.teksttyper, request.minimumAntall)
-                }
-            } else {
-                varseltekstRepository.tellAntallVarselteksterTotalt(
-                    teksttyper = request.teksttyper,
-                    varseltype = request.varseltype,
-                    startDato = request.startDato,
-                    sluttDato = request.sluttDato,
-                    inkluderStandardtekster = request.inkluderStandardtekster
-                ).let {
-                    ExcelFileWriter.totaltAntallToExcelSheet(it, request.teksttyper, request.minimumAntall)
-                }
+            processRequest(request).let { workbook ->
+                fileStore[fileId] = ExcelFile.ready(filename, workbook)
             }
         }
 
@@ -69,6 +50,104 @@ class VarseltekstQueryService(
         return excelFile
     }
 
+    private fun processRequest(request: DownloadRequest): Workbook {
+        return if (request.detaljert) {
+            varseltekstRepository.tellAntallVarseltekster(
+                teksttyper = request.teksttyper,
+                varseltype = request.varseltype,
+                startDato = request.startDato,
+                sluttDato = request.sluttDato,
+                inkluderStandardtekster = request.inkluderStandardtekster
+            ).let {
+                sladdDetaljerteTekster(it, request.minimumAntall)
+            }.let {
+                ExcelWorkbookWriter.antallToExcelSheet(it)
+            }
+        } else {
+            varseltekstRepository.tellAntallVarselteksterTotalt(
+                teksttyper = request.teksttyper,
+                varseltype = request.varseltype,
+                startDato = request.startDato,
+                sluttDato = request.sluttDato,
+                inkluderStandardtekster = request.inkluderStandardtekster
+            ).let {
+                sladdTekster(it, request.minimumAntall)
+            }.let {
+                ExcelWorkbookWriter.totaltAntallToExcelSheet(it)
+            }
+        }
+    }
+
+    private fun sladdTekster(totaltAntall: TotaltAntall, minAntall: Int): TotaltAntall {
+
+        val (beholdes, sladdes) = totaltAntall.permutasjoner.partition {
+            permutasjon -> permutasjon.antall >= minAntall
+                || permutasjon.tekster.none { it.innhold == Tekst.Innhold.Egendefinert }
+        }
+
+        val sladdet = sladdes.map { permutasjon ->
+            TotaltAntall.Permutasjon(
+                antall = permutasjon.antall,
+                tekster = permutasjon.tekster.map { tekst ->
+                    if (tekst.innhold == Tekst.Innhold.Egendefinert) {
+                        Tekst(null, Tekst.Innhold.Sladdet)
+                    } else {
+                        tekst
+                    }
+                }
+            )
+        }.groupBy {
+            it.tekster
+        }.map { (tekster, permutasjoner) ->
+            TotaltAntall.Permutasjon(
+                antall = permutasjoner.sumOf { it.antall },
+                tekster = tekster
+            )
+        }
+
+        return (sladdet + beholdes)
+            .sortedByDescending { it.antall }
+            .let { TotaltAntall(totaltAntall.teksttyper, it) }
+    }
+
+
+    private fun sladdDetaljerteTekster(detaljertAntall: DetaljertAntall, minAntall: Int): DetaljertAntall {
+
+        val (beholdes, sladdes) = detaljertAntall.permutasjoner.partition { permutasjon ->
+            permutasjon.antall >= minAntall
+                || permutasjon.tekster.none {it.innhold == Tekst.Innhold.Egendefinert }
+        }
+
+        val sladdet = sladdes.map { permutasjon ->
+            DetaljertAntall.Permutasjon(
+                varseltype = permutasjon.varseltype,
+                produsent = permutasjon.produsent,
+                antall = permutasjon.antall,
+                tekster = permutasjon.tekster.map { tekst ->
+                    if (tekst.innhold == Tekst.Innhold.Egendefinert) {
+                        Tekst(null, Tekst.Innhold.Sladdet)
+                    } else {
+                        tekst
+                    }
+                }
+            )
+        }.groupBy {
+            Triple(it.varseltype, it.produsent, it.tekster)
+        }.map { (vpt, permutasjoner) ->
+            DetaljertAntall.Permutasjon(
+                varseltype = vpt.first,
+                produsent = vpt.second,
+                antall = permutasjoner.sumOf { it.antall },
+                tekster = vpt.third
+            )
+        }
+
+
+        return (sladdet + beholdes)
+            .sortedByDescending { it.antall }
+            .let { DetaljertAntall(detaljertAntall.teksttyper, it) }
+    }
+
     data class ExcelFile(
         val filename: String,
         var workbook: Workbook?,
@@ -80,6 +159,11 @@ class VarseltekstQueryService(
             fun waiting(filename: String) = ExcelFile(
                 filename = filename,
                 workbook = null,
+            )
+
+            fun ready(filename: String, workbook: Workbook) = ExcelFile(
+                filename = filename,
+                workbook = workbook
             )
         }
     }
